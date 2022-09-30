dea5e10f5d37d889f0da6226029f08f531d74e15
richard <muyuxiaoke@163.com>
2022-09-30 13:30:14 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## 顺序
++ 实战使用
++ 优化
++ 原理(工作流) AST 抽象语法树
++ loader
++ plugin tapable
++ hmr (热更新)实现原理
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+      publicPath: '/assets'
+    },
+    // static: ['dist'],
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/, use: 'raw-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./src/index.html`
+    })
+  ]
+}
